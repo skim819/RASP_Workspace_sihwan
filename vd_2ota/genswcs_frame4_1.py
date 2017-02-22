02739d80a7a9ed36371d00fe9d9d49717dee8a35
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2017-02-22 15:48:56 -0500

    commit

--- vd_2ota/genswcs_frame4_1.py
+++
@@ -1,1 +0,0 @@
-						elif from_sub_name in ['mite2[0]','mult[0]','bias_gen[0]','ladder_filter[0]','dendiff[0]','hhneuron[0]','speech[0]','dendrite_4x4[0]','ramp_fe[0]','hhn_debug[0]'
--- vd_2ota/genswcs_vd_2ota_5.py
+++
@@ -1,2 +0,0 @@
-				elif subckt in ['vd_2ota']:
-					key=ports[1]
